3614976150242495f6bddf308183c3a332f65f9d
Abraham Linkon <37959190+linkon63@users.noreply.github.com>
2023-01-10 19:28:44 +0600

    my chat app created the routes and setup the projects

---
+++ src/components/Routes/routes.js
@@ -0,0 +1,14 @@
+export const authRoutes = [
+    {
+        path: "/",
+        element: <h1>Root</h1>,
+    },
+    {
+        path: "/home",
+        element: <h1>Home</h1>,
+    },
+    {
+        path: "/about",
+        element: <h1>About</h1>,
+    },
+]
